7c33ace10a5f5536a5ee97b19d88c7279c7ca21d
Huey <2148355311@qq.com>
2025-05-15 14:28:32 +0800

    登录页创建

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import homePageFramework from '../components/HomePageFramework/homePageFramework.vue'
 import homeView from '../views/Home/homeView.vue'
+import Login from '@/views/LoginView/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,12 @@ const router = createRouter({
       name: 'homeView',
       component: homeView,
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+
   ],
 })
 
---
+++ src/views/LoginView/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <homePageFramework>
+    <template #homePage>
+
+      <div>
+
+
+        <h2>
+          登录页222
+        </h2>
+
+      </div>
+    </template>
+  </homePageFramework>
+</template>
+
+<script setup>
+import homePageFramework from '@/components/HomePageFramework/homePageFramework.vue'
+</script>
+
+<style scoped lang="scss"></style>
