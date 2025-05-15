3334fc34e3229aa1dadc026fa98099b33c706d22
Stardust2216 <2061825936@qq.com>
2025-05-16 06:24:17 +0800

    商城页面编写

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import homePageFramework from '../components/HomePageFramework/homePageFramework
 import homeView from '../views/Home/homeView.vue'
 import Login from '@/views/LoginView/Login.vue'
 import SignUp from '@/views/LoginView/SignUp.vue'
+import Shop from '@/views/ShopView/Shop.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +28,11 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: SignUp,
+    },
+    {
+      path: '/shop',
+      name: 'shop',
+      component: Shop,
     },
 
   ],
